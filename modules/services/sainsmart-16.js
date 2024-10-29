e0c3e353a676c9b5d2dc81e5877d3a0969629bb7
Mat Janson Blanchet <mat.jb@jansensan.net>
2024-10-28 21:34:42 -0400

    Handled rpio exit by disabling pins

--- modules/services/sainsmart-16.js
+++ modules/services/sainsmart-16.js
@@ -23,10 +23,29 @@ module.exports = {
 function init() {
   console.info('--- sainsmart init ---');
 
+  rpio.init({ close_on_exit: false });
+
   // set initial pin states -- should all be off (no current passing)
   PINS.forEach(pin => {
     rpio.open(pin, rpio.OUTPUT, RelayState.OFF);
   });
+
+  process.on('exit', () => {
+    disableAllPins();
+    rpio.exit();
+  });
+}
+
+function enableAllPins() {
+  console.info('--- sainsmart enableAllPins ---');
+
+  PINS.forEach(pin => rpio.write(pin, RelayState.ON));
+}
+
+function disableAllPins() {
+  console.info('--- sainsmart disableAllPins ---');
+
+  PINS.forEach(pin => rpio.write(pin, RelayState.OFF));
 }
 
 function enablePinsSequentially() {
